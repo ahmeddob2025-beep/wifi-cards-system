332ff9511570e353f5b4b676f55d38df7248d3c4
ahmeddob2025-beep <ahmeddob2025@gmail.com>
2026-08-03 01:23:55 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
-const CACHE_NAME = 'wifi-cards-v2';
+const CACHE_NAME = 'wifi-pro-v1';
 const urlsToCache = [
-  '/wifi-cards-system/',
-  '/wifi-cards-system/index.html',
-  '/wifi-cards-system/manifest.json'
+  '/wifi-cards-system/?v=pro',
+  '/wifi-cards-system/index.html?v=pro',
+  '/wifi-cards-system/manifest.json?v=pro'
 ];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
